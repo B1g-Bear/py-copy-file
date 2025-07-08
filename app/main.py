0204750226a95195059e18804a000802ba09c051
Andrii Novikov <forytube2023@gmail.com>
2025-07-08 13:52:04 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,6 +5,9 @@ def copy_file(command: str) -> None:
     source_file_name, destination_file_name = parts[1], parts[2]
     if source_file_name == destination_file_name:
         return
-    with open(source_file_name, "r") as source_file_object, \
-         open(destination_file_name, "w") as destination_file_object:
-        destination_file_object.write(source_file_object.read())
+    try:
+        with open(source_file_name, "r") as source_file_object, \
+             open(destination_file_name, "w") as destination_file_object:
+            destination_file_object.write(source_file_object.read())
+    except FileNotFoundError:
+        return
