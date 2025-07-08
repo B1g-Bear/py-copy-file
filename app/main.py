ddbe45758dcac18e51edf9b4715d2897488ec52f
Andrii Novikov <forytube2023@gmail.com>
2025-07-08 13:49:47 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+    source_file_name, destination_file_name = parts[1], parts[2]
+    if source_file_name == destination_file_name:
+        return
+    with open(source_file_name, "r") as source_file_object, open(destination_file_name, "w") as destination_file_object:
+        destination_file_object.write(source_file_object.read())
